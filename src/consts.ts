bc6a8373144068341f30850726cf47b358cb9afc
lcfd <9001053+fedriz@users.noreply.github.com>
2023-09-07 19:03:35 +0200

    It works!!!

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
+export const SITE_TITLE = "Speck&Tech";
+export const SITE_DESCRIPTION = "Tasty events";
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+const SOCIALS = { twitter: { icon: "", text: "Twitter", url: "" } };
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,24 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+const events = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    // date: z.coerce.date(),
+    categories: z.string(),
+  }),
 });
 
-export const collections = { blog };
+const blog = defineCollection({
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
+});
+
+export const collections = { blog, events };
