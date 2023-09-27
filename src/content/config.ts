d4670885f0deecca50e42eaaace4c2361c981e53
Andrea Leone <andrealeone@hey.com>
2023-09-27 17:29:25 +0200

    General cleanup and ultra-basic styling

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,22 +3,9 @@ import { defineCollection, z } from "astro:content";
 const events = defineCollection({
   schema: z.object({
     title: z.string(),
-    date: z.date(),
-    // date: z.coerce.date(),
+    date: z.date(), // z.coerce.date(),
     categories: z.string(),
   }),
 });
 
-const blog = defineCollection({
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
-});
-
-export const collections = { blog, events };
+export const collections = { events };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      accent: "#e7424f", // rgb(231, 66, 79)
+      accentDark: "#b9353f"
+    },
     extend: {},
   },
   plugins: [],
