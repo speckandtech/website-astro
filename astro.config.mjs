72f3f03d748c7423df61c2e7294e1280894eb989
Luca Fedrizzi <9001053+lcfd@users.noreply.github.com>
2023-10-04 21:11:30 +0200

    Add new config to astro.config and github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://speckand.tech",
+  // site: "https://speckand.tech",
+  site: 'https://speckandtech.github.io',
+  base: '/website-astro',
   integrations: [mdx(), sitemap(), tailwind()],
 });
